b2ca850afec864c804bd873213f68225b9f573ac
oibuoye <ope4sure1@gmail.com>
2020-01-05 22:55:43 +0100

    Sample implementation of getting configuration settings using IOptions in Asp.net Core

---
+++ Configurations/TokenConfig.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DependencyInjectionInCSharp.Configurations
+{
+    public class TokenConfig
+    {
+        public string Key { get; set; }
+        public string ExpirePeriod { get; set; }
+    }
+}
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,20 +6,25 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using DependencyInjectionInCSharp.Models;
+using DependencyInjectionInCSharp.Configurations;
+using Microsoft.Extensions.Options;
 
 namespace DependencyInjectionInCSharp.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly TokenConfig _tokenConfig;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IOptions<TokenConfig> tokenConfig)
         {
             _logger = logger;
+            _tokenConfig = tokenConfig.Value;
         }
 
         public IActionResult Index()
         {
+            string getTokenKey = _tokenConfig.Key;
             return View();
         }
 
